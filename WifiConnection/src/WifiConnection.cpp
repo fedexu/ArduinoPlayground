921bfded9a63a99d3f53124a98e06f5c38d8d767
Federico Peruzzi <federico.xu125@gmail.com>
2021-05-05 12:05:24 +0200

    wifi Connecting works

--- WifiConnection/src/WifiConnection.cpp
+++ WifiConnection/src/WifiConnection.cpp
@@ -1,33 +1,44 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <BlinkLed2.h>
 
-char ssid[] = "Vodafone-FedeXu";     //  your network SSID (name)
-char pass[] = "NeverMind1";    // your network password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
-
+int wifiStatus = WL_IDLE_STATUS; // the Wifi radio's status
+#define MAX_SRV_CLIENTS 1
+const char *ssid = "Vodafone-FedeXu"; //  your network SSID (name)
+const char *password = "NeverMind1";  // your network password
+WiFiMulti wifiMulti;
 
 void blinkLed2(int seconds);
 
-void setup() {
+void setup()
+{
   // initialize serial:
   Serial.begin(9600);
   BlinkLed2::setupLed();
   // attempt to connect using WPA2 encryption:
   Serial.println("Attempting to connect to WPA network...");
-  status = WiFi.begin(ssid, pass);
-  // if you're not connected, stop here:
-  if ( status != WL_CONNECTED) {
-    Serial.println("Couldn't get a wifi connection");
-  }
+  wifiMulti.addAP(ssid, password);
   // if you are connected, print out info about the connection:
-  else {
+  wifiStatus = wifiMulti.run();
+  if (wifiStatus == WL_CONNECTED)
+  {
     Serial.println("Connected to network");
+    Serial.println("");
+    Serial.print("WiFi connected ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  // if you're not connected, stop here:
+  if (wifiStatus != WL_CONNECTED)
+  {
+    Serial.println("WiFi connect failed");
+    delay(1000);
+    ESP.restart();
   }
 }
 
-void loop() {
+void loop()
+{
   BlinkLed2::blinkLed(1000, 1000);
 }
-
-
